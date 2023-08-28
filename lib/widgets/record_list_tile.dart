edff204a69e3beb5828804041a44788287327be1
Taner Topal <tanertopal@gmail.com>
2023-08-28 16:25:31 +0300

    weight card added

--- lib/widgets/record_list_tile.dart
+++ lib/widgets/record_list_tile.dart
@@ -16,6 +16,7 @@ class RecordListTile extends StatelessWidget {
 
 
     return Card(
+      elevation: 3,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16)
       ),
